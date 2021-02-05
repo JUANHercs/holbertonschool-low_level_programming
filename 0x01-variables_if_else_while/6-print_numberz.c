1da49510132dea67e3631cf70e195b6f189984aa
JUANHercs <git>
2021-02-05 21:16:12 +0000

    Num_ASCII

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,17 +9,9 @@
 int main(void)
 {
 	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-		printf("%d is negative\n", n);
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
 	return (0);
 }
