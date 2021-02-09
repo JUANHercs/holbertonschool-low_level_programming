bc9aeaaa741f608b2a638f2f105fb404458c3e52
JUANHercs <git>
2021-02-09 02:43:36 +0000

    ElAlphaPro

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * print_alphabet - prototype print
+ * Return: nose
+ */
+
+void print_alphabet(void)
+{
+	int w;
+
+	for (w = 'a'; w <= 'z'; w++)
+{
+	_putchar(w);
+}
+	_putchar('\n');
+}
