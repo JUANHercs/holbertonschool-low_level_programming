313bd9d3c00f74a4a6bc7b5906021af5063c5db5
JUANHercs <git>
2021-02-09 18:31:07 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+* prototype  - int _islower(int c)
+* Return: 1 if is c, 0 oyherwise[D[D[D
+*/
+
+int _islower(int c)
+{
+	int a, b;
+
+	for (a = 'a'; a >= 'z'; a++)
+	{
+		if (a == c)
+		{
+			b = 1;
+			return (b);
+		}
+	 }
+	return (0);
+}
