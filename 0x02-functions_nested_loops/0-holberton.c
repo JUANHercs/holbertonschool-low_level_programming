49432c27391259fde9211095ab14aff125a7f7f3
JUANHercs <git>
2021-02-09 01:19:20 +0000

    use_of_prototype

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include"holberton.h"
+
+/**
+* main - Entry point
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+	_putchar(72);
+	_putchar('O');
+	_putchar(76);
+	_putchar('B');
+	_putchar(69);
+	_putchar('R');
+	_putchar(84);
+	_putchar('O');
+	_putchar(78);
+	_putchar('\n');
+
+	return (0);
+}
