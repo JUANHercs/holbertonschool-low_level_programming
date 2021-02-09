19f156e2441bbbdbb54833bf22eaf3b0d8e6d583
JUANHercs <git>
2021-02-09 22:53:34 +0000

    LastDigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,20 @@
 #include"holberton.h"
+
 /**
- * print_last_digit - Function tht returns the las value of a number
- * @n: the argument of the function a number int
- * Return: the las number int
- */
+ * print_last_digit -  functio that print the last digit
+ *@n: the argument
+ *Return: the value of the last digit
+ **/
 
 int print_last_digit(int n)
 {
 	int l;
 
 	l = n % 10;
-	_putchar(l + '0');
+	if (l < 0)
+	{
+		l = -l;
+	}
+	_putchar (l + '0');
 	return (l);
 }
