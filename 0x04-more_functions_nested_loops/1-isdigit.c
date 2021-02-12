2a7a89e3c924dde2b695a2a0a4b5044310ba1c02
JUANHercs <git>
2021-02-12 17:51:28 +0000

    checks_0to9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _isdigit - checks for digit 0 to 9
+ *@c: is the argument of the function
+ *Return: 1 if c is a digit 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+
+	while (c >= '0' && c <= '9')
+{
+	return (1);
+}
+return (0);
+}
