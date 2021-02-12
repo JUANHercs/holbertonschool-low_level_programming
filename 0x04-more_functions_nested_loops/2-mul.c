b3f540562ab7485d7715d07948ee1081614e8b5c
JUANHercs <git>
2021-02-12 18:04:11 +0000

    Multiplay

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+/**
+ * mul - multiplies two integers
+ *@a: is the argument an integer
+ *@b: is the argument of the function
+ *Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
