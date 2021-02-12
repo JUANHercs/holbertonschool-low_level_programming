7770cb126f923c4f8d0b6807c2b621ebd47c26df
JUANHercs <git>
2021-02-12 17:33:59 +0000

    Char_Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _isupper - checks for digit 0 to 9
+ *@c: is the argiment of the function
+ *Return: 1 if c is a digit 0 otherwise
+ */
+
+int _isupper(int c)
+{
+
+	while (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+return (0);
+}
