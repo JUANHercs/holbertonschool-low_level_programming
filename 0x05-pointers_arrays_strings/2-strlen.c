de8cc4948885e35bc69da718dc3cf67f0c28ffb6
JUANHercs <git>
2021-02-17 00:02:20 +0000

    lenght_with_pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_strlen - prototype name
+ *@s: is the value of the array
+ *Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+return(n);
+}
