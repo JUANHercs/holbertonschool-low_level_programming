8806b908b91f060d6cd31fa1052ccd49b472c335
JUANHercs <git>
2021-02-17 22:03:13 +0000

    half_string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,21 @@
 
 void puts_half(char *str)
 {
-	int  n;
+	int  h, n, length_of_string;
 
 	for (n = 0; str[n] != '\0'; n++)
 	{
 	}
-	length_of_string = n-1;
+	length_of_string = n;
 	if (length_of_string % 2 != 0)
 		{
-			_putchar(str[n]);
+			h = (length_of_string - 1) / 2;
 		}
+	h = n / 2;
+	while (str[h] != '\0')
+	{
+		_putchar(str[h]);
+		h++;
 	}
 	_putchar('\n');
 }
