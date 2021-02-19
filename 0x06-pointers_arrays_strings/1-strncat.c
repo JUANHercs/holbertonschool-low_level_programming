0a0f779b12a5552d36721b631596d68852a1e102
JUANHercs <git>
2021-02-19 17:47:47 +0000

    conatenate2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  **_strncat - prototype
@@ -12,14 +11,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
 	int i;
+	int ii = 0;
+	while (dest[ii] != '\0')
+	{
+		ii++;
+	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[dest_len + i] = src[i];
+		dest[ii + i] = src[i];
 	}
-	dest[dest_len + i] = '\0';
+	dest[ii + i] = '\0';
 
 	return (dest);
 }
