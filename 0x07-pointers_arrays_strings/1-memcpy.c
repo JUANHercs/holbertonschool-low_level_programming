ad498d235b570fe18b5706b1e00a484aad9d5199
JUANHercs <git>
2021-02-22 23:33:30 +0000

    copy_n_bytes_to_another_array

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - function fills n bytes of the memory area
+ * @dest: memory area dest
+ * @src: memory area source
+ * @n: number of bytes to be copied
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	return (dest);
+}
