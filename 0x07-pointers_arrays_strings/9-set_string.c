da299f1db81b0becd03ae7c47eabb8696247e6f6
JUANHercs <git>
2021-02-23 23:33:12 +0000

    pointer_redirect

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - se value pointer to a char
+ * @s: direccion de un pointer que apunta a un char
+ * @to: pointer of char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to){
+	*s = to;
+}
