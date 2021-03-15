1502d66c781e0b76d5e714c7404cf4e304b3d221
JUANHercs <git>
2021-03-15 19:09:43 +0000

    Header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VARIADIC_FUNC_H
+#define _VARIADIC_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
