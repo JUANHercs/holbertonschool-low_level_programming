92b128ef946ac8c2e3f389b79600367ad02c5664
JUANHercs <git>
2021-03-16 18:50:26 +0000

    unused

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *@argc: number of command line arguments
+ *@argv: array of command line arguments
+ *Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
