aac9ce488514d8400433a0cf6d66a23b08bb4122
JUANHercs <git>
2021-03-24 17:12:52 +0000

    end_node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,16 +9,18 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	unsigned int c;
 	list_t *new = (list_t *)malloc(sizeof(list_t));
 	list_t *last = *head;
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	for (c = 0; str[c] != '\0'; c++);
+		new->len = c;
 	if (*head == NULL)
 	{
 		*head = new;
 		new->next = NULL;
-		return(NULL);
+		return (NULL);
 	}
 	while (last->next != NULL)
 		last = last->next;
