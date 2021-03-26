4c7f10f1c464d3c79ec9e1adc44518f7e02f882e
JUANHercs <git>
2021-03-26 17:18:18 +0000

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, m;
+
+	for (n = '0'; n <= '8'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			if (n != m && n < m)
+			{
+				putchar(n);
+				putchar(m);
+				if (n == '8' && m == '9')
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}
