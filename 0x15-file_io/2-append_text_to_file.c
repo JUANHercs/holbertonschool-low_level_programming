3240a5e026edc1cfbd3020f72224ed1bd2574576
JUANHercs <git>
2021-03-30 04:43:57 +0000

    append_text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *append_text_to_file - append text at the end of a file
+ *@filename: name of the file
+ *@text_content: Null terminated string to add at the end of file
+ *Return: -1 f fails , 1 if success
+ **/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
