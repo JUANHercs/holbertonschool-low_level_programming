877422f8a63045643a264d32584b22c09e4351dd
JUANHercs <git>
2021-03-30 04:48:22 +0000

    append_text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,13 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	ssize_t fd, wr;
+	int len = 0;
 
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 }
