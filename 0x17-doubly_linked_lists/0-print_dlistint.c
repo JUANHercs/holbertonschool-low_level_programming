39e701d158053e0a5de16d34393a74d566e07abe
JUANHercs <juan.hercs@gmail.com>
2021-05-02 21:54:15 +0000

    print_list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int nodes = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
 	}
-	return(nodes);
+	return (nodes);
 }
