20bbb38f05cb85786bab4a8d6df941fd94a2d47e
“JUANHercs” <juan.hercs@gmail.com>
2021-05-31 01:43:19 -0500

    clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,22 +7,22 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d, h, m;
+int a, b, c, d, h, m;
 
-    for (h = 0; h <= 23; h++)
-    {
-        for (m = 0; m <= 59; m++)
-        {
-            a = h / 10;
-            b = h % 10;
-            c = m / 10;
-            d = m % 10;
-            _putchar(a + '0');
-            _putchar(b + '0');
-            _putchar(':');
-            _putchar(c + '0');
-            _putchar(d + '0');
-            _putchar('\n');
-        }
-    }
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+a = h / 10;
+b = h % 10;
+c = m / 10;
+d = m % 10;
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+}
+}
 }
